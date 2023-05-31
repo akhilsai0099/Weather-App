4b638cf944d6cd8114d351e6da36f0b8576a0c95
akhilsai0099 <akhilsai0099@gmail.com>
2023-05-31 22:27:05 +0530

    Removed a div causing misalignment of input

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -46,34 +46,33 @@ const Autocomplete = () => {
     }
 
     return (
-        <div className='flex flex-col justify-center text-center '>
-            <div className='w-96'>
+        <div className='flex flex-col justify-center text-center w-fit'>
 
-                <label htmlFor="search">Search for your city</label>
-                <form onSubmit={handleSubmit} className='flex gap-2'>
-                    <input
-                        type="text"
-                        ref={inputRef}
-                        name="search"
-                        id="city-search"
-                        placeholder='Enter the city name'
-                        className=' w-60 border-blue-400 rounded-xl p-5 focus:outline-none focus:outline-orange-400 focus:border-orange-400 focus:rounded-b-none'
-                        onInput={handleInput}
-                        value={inputValue}
-                        onChange={(e) => { setInputValue(e.target.value) }}
-                        autoComplete='off'
-                        onFocus={() => { setIsFocused(true) }}
-                        onBlur={() => {
-                            if (!preventBlurRef.current) {
-                                setIsFocused(false);
-                            } else {
-                                preventBlurRef.current = false;
-                            }
-                        }} />
 
-                    <input type="submit" value="Search" className='bg-[#3b3b3b] items-center w-20 rounded-lg hover:bg-blue-800' />
-                </form>
-            </div>
+            <label htmlFor="search">Search for your city</label>
+            <form onSubmit={handleSubmit} className='flex gap-3'>
+                <input
+                    type="text"
+                    ref={inputRef}
+                    name="search"
+                    id="city-search"
+                    placeholder='Enter the city name'
+                    className=' w-60 border-blue-400 rounded-xl p-5 focus:outline-none focus:outline-orange-400 focus:border-orange-400 focus:rounded-b-none'
+                    onInput={handleInput}
+                    value={inputValue}
+                    onChange={(e) => { setInputValue(e.target.value) }}
+                    autoComplete='off'
+                    onFocus={() => { setIsFocused(true) }}
+                    onBlur={() => {
+                        if (!preventBlurRef.current) {
+                            setIsFocused(false);
+                        } else {
+                            preventBlurRef.current = false;
+                        }
+                    }} />
+
+                <input type="submit" value="Search" className='bg-[#3b3b3b] items-center w-20 rounded-lg hover:bg-blue-800' />
+            </form>
 
             <div className='relative'>
                 <div className='absolute top-0 left-0 w-60 '>
