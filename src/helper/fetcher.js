4b074e91f40bb5e1e7afb0df34ab3923cdd3ebd1
akhilsai0099 <akhilsai0099@gmail.com>
2023-05-31 20:31:56 +0530

    added _redirects

--- src/helper/fetcher.js
+++ src/helper/fetcher.js
@@ -1,8 +1,20 @@
 import axios from "axios";
 
 const fetcher = async (city) => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${import.meta.env.VITE_WEATHER_API_URL}`
+    const geocoderResponse = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=0f690fb60d42aa91dcd2959c81b5fe8b`);
+    const geocoderData = geocoderResponse.data;
+    let filteredData = geocoderData;
+    if (geocoderData.length > 1) {
+        filteredData = geocoderData.filter((item) => {
+            return item.country === "IN";
+        })
+    }
+    console.log(geocoderData)
+    const lat = filteredData[0]["lat"]
+    const lon = filteredData[0]["lon"]
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${import.meta.env.VITE_WEATHER_API_URL}`
     const weatherResponse = await axios.get(url)
+
     const selectedFields = {
         main: weatherResponse.data.main,
         weather: weatherResponse.data.weather,
